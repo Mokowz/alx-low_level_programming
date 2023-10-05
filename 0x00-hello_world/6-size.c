3bd33c9fd9a0a8ae50f4aa8e3876d1af852b028e
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-05 02:22:32 -0700

    Remove whitespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %zu byte(s)", sizeof(float));
+	printf("Size of a char: %d byte(s)\n",(int) sizeof(char));
+	printf("Size of a int: %d byte(s)\n",(int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n",(int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n",(int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n",(int) sizeof(float));
 
 	return (0);
 }
