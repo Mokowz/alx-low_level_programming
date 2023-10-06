bf6af5c633b11f9b7ee69063794802dd22567369
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-06 10:30:58 -0700

    Print single digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print single numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n='0'; n<='9'; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
