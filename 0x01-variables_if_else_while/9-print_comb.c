f52f869ad1d4f25724309bc228044c4f295b349d
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-06 10:42:00 -0700

    Print all numbers separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers separated by commas
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c='0'; c<='9'; c++)
+	{
+		putchar(c);
+
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
