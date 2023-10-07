bdc2da8bd9db2bffb92d7b89293c20b8015cc1b1
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-07 04:08:45 -0700

    Print combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,15 +16,18 @@ int main(void)
 			if (c < i)
 			{
 				putchar(c);
-				putchar(i);				
+				putchar(i);
+
 				if (c != '8' || (c == '8' && i != '9'))
 				{
 					putchar(',');
 					putchar(' ');
+	
 				}
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
