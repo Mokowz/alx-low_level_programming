a713198cd1a60418b0a81c09a954c5db936cee2e
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 10:06:54 -0700

    Calculate sum of even numbered fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Add the first 50 even fibonacci
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+	long j = 1, k = 2;
+	long sum = k;
+
+	while (k + j  < 4000000)
+	{
+		k += j;
+		j = k - j;
+		if (k % 2 == 0)
+		{
+			sum += k;
+		}
+	
+		i++;
+	}
+	printf("Sum = %ld\n", sum);
+
+	return (0);
+}
