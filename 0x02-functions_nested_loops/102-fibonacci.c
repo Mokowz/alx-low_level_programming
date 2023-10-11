2db07e59cfac8e4ef431ebedb040055c05ca6767
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 09:46:36 -0700

    Print fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 50 fibonacci
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0, j = 1, k = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			print("%d", j);
+		}
+		else if (i == 0)
+		{
+			printf(", %d", k);
+		}
+		else
+		{
+			k += j;
+			j = k - j;
+			
+			printf(", %d", k);
+		}
+		i++;
+	}
+	printf("\n");
+
+	return (0);
+}
