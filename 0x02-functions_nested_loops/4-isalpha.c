8cf8aa3cd2cd106ca619563762d5cabb7ed540f1
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 00:19:36 -0700

    Check if a character is a letter

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if c is a letter
+ *
+ * Return: 1 if c is a letter
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
