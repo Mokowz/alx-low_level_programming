5fc4e29bea4fab070e05e949c1e799e2292c1fc0
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 03:48:44 -0700

    Add description

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,9 +2,10 @@
 
 /**
  * print_sign: Print sign of a number
+ * Description: Print sign of number
  * @n: n is a number
  *
- * Return: 1 if n is greater than 0
+ * Return: 1 for positive, 0 for zero, -1 for negative
  */
 int print_sign(int n)
 {
