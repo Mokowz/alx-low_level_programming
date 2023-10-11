25bfd90271e83a0dcf3d9cdaee9f8b4cba399e70
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 03:37:00 -0700

    Add description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,12 +2,15 @@
 
 /**
  * add - Adds two integers
+ * @n: integer 1
+ * @m: integer 2
+ * Description: Two integers return sum
  *
  * Return: integers
  */
 int add(int n, int m)
 {
 	int sum = n + m;
-	
+
 	return (sum);
 }
