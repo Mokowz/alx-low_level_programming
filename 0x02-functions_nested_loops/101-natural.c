c9ed999244841674aaecce1cfa28a5bf288fc81a
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 09:34:44 -0700

    Add prime numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - List all natural numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int sum = 0;
+	int i = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+		i++;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
