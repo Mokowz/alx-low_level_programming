c2ce418f5008f24d10d68f9edb7dfc2161f74e8e
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-11 00:29:42 -0700

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - Compute absolute value of a number
+ *
+ * Return: Number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
