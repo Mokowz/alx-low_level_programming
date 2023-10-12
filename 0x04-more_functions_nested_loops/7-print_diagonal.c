82232374d0b7672cdfc2ff0e4cab9cccab8bf515
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-12 08:15:41 -0700

    Add spaces

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,13 @@
  */
 void print_diagonal(int n)
 {
-	int line = 0;
+	int line = 0, spaces;
 
 	if (n > 0)
 	{
 		while (line < n)
 		{
-			for (int spaces = 0; spaces < line; spaces++)
+			for (spaces = 0; spaces < line; spaces++)
 				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
