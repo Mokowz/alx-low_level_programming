d94ecc64b0a11f58c2c06a43c11c4d01035ca1d4
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-12 06:24:11 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - pritn numbers from 0 to 9
+ *
+ * Return: Null
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
