64406954df12cfde24ad342ebf3e0245d1d2fd3f
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-16 03:53:35 -0700

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
