1c1d5f782af8189fe412be28408e1223bccfe6e3
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-16 06:12:59 -0700

    Copy string to another string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy string from src to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: pointer to character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
