8fe54ee9a2b096435d7363bece01d63d4456ca23
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-16 05:09:44 -0700

    Print first n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print n elements of an array
+ * @a: array to be used
+ * @n: size to be printed
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			printf("%d", a[i]);
+		else
+			printf("%d, ", arr[i]);
+	}
+}
