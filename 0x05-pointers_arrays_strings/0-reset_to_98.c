35a465ae564572612e4b928bd9712404ba0bf134
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-16 03:33:09 -0700

    Add a reset to 98 function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - This will reset the value of a pointer to 98
+ * @n: n is the pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
