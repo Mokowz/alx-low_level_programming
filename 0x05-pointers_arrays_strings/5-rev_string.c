a3dcc828ea042f3abe45a62023858cfc5b4f010d
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-16 05:49:55 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#
+#include <string.h>
 #include "main.h"
 
 /**
@@ -9,3 +9,31 @@
  * Return: void
  */
 void rev_string(char *s)
+{
+	int length, i = 0;
+
+	length = strlen(s) - 1;
+
+	while (length > i)
+	{
+		swap_char(s +length, s + i);
+		i++;
+		length--;
+	}
+}
+
+/**
+ * swap_char - swap characters
+ * @a: first character
+ * @b: second character
+ *
+ * Return: void
+ */
+void swap_char(char *a, char *b)
+{
+	char temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
