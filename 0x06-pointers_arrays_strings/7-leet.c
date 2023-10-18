b138f5cd46c963dd6b4803189877dceb68ad66a9
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-18 04:32:39 -0700

    Encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - encode a string
+ * @s: String to be encoded
+ *
+ * Return: String
+ */
+char *leet(char *s)
+{
+	char orig[] = 'aAeEoOtTlL';
+	char enc[] = '4433007711';
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; orig[j] != '\0'; j++)
+		{
+			if (s[i] == orig[j])
+				s[i] = enc[j];
+		}
+	}
+
+	return (s);
+}
