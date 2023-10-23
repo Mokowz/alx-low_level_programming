1532d08e67416c64432058400b44479f3ac9a86b
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-23 06:39:50 -0700

    Add flags

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,16 +9,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, len = 0;
+	unsigned int i, j, flag, len = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				len += 1;
+				flag = 1;
+			}
 		}
+		if (flag == 0)
+			return (len);
 	}
 
-	return (len);
+	return (0);
 }
