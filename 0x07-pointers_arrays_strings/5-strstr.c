a130e90ddf84ddd6d98354f47ba9ba8aedf5aa83
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-23 12:43:59 -0700

    Solve compile error

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: String
+ * @needle: Substring
+ *
+ * Return: Pointer to beginning of located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,3 +4,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 void print_diagsums(int *a, int size);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
