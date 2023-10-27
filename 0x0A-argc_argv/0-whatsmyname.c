7dfaeebc73a6a5ea4646fcca0acbd63c38eaf6d6
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-27 09:57:48 -0700

    Print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print name of program
+ * @argc: Argument count
+ * @argv: Array of arguments
+ *
+ * Return: 0
+ */
+int main(int __atribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
