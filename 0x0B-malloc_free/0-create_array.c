600e171b896d885261c2011accbca15926641dbe
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-30 03:04:27 -0700

    Allocate space dynamically and initialize it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create an array of chars and initialize it with values
+ * @size: Size of array
+ * @c: Character
+ *
+ * Return: Pointer to character
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr = NULL;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * sizeof(char));
+
+	for (unsigned int i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
