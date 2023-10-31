1e5cf4c60c2576214d83d5b7109f471fd6572edf
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-10-31 01:05:47 -0700

    Free allocated space to the 2D

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free space allocated to the 2D array
+ * @grid: Array
+ * @height: Row
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
