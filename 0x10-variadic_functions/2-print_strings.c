7f08fccfade9ef2993f6031fbeb6063f5a343c41
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-11-09 14:57:25 -0800

    Fix compile errors

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator != NULL && i > 0)
 			printf("%s", separator);
 
-		if (str == NULL)
+		if (s == NULL)
 			printf("(nil)");
 		else
 			printf("%s", s);
