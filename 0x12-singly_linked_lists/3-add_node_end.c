ff1f6ef1d09aedca72d9924710d04950a7976053
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-11-16 07:38:07 -0800

    Fix issues

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,11 +12,17 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *newNode = malloc(sizeof(list_t));
 	list_t *p;
 
+	p = *head;
+
 	if (newNode == NULL)
 		return (NULL);
 
 	if (str == NULL)
 		return (NULL);
+
+	newNode->str = strdup(str);
+        newNode->len = strlen(str);
+        newNode->next = NULL;
 
 	if (*head == NULL)
 	{
@@ -24,16 +30,9 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (newNode);
 	}
 
-	p = *head;
-
 	while (p->next != NULL)
 		p = p->next;
 
 	p->next = newNode;
-
-	newNode->str = strdup(str);
-	newNode->len = strlen(str);
-	newNode->next = NULL;
-
-	return (newNode);
+	return (*head);
 }
