6fb31bb1ddefabbea01a6b4bca3f24110c27a60b
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-11-16 07:43:58 -0800

    Free alist

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free a list
+ * @head: HEad
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t p;
+
+	while (*head != NULL)
+	{
+		p = head->next;
+		free(head->str);
+		free(head);
+		head = p;
+	}
+}
