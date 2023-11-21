77c84a88fb49f86db387f4d5b82f01a3300c583e
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-11-21 00:29:51 -0800

    Print all elements of the linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - print elements of a linked list
+ * @h: header
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count += 1;
+	}
+
+	return (count);
+}
