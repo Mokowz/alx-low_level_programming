407800c762c456f50c45da90fd08a7724755fb93
Ronny Mokaya <ronniemokaya30@gmail.com>
2023-11-21 02:42:07 -0800

    Sum data in linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sums all data
+ * @head: First node
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *temp;
+	int sum = 0, n;
+
+	if (head == NULL)
+		return (NULL);
+
+	temp = head;
+
+	while (temp != NULL)
+	{
+		n = temp->n;
+		sum += n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
