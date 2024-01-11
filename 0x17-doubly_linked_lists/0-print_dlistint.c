33b308f967cafd8e0d99431ea2c86789288944ba
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-11 01:36:22 -0800

    Print all elements

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - return number of nodes
+ * @h: Header
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	
+	return (count);
+}
