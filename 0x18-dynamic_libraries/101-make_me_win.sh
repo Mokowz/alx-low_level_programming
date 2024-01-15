38b50978e535b24cf11052e3dc70258866325f2e
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-15 04:59:17 -0800

    Rectify the link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -q -O /tmp/gigalib.so "https://raw.githubusercontent.com/Hagos2022/alx-low_level_programming/master/0x18-dynamic_libraries/gigalib.so"
-export LD_PRELOAD=/tmp/gigalib.so
+wget -P /tmp "https://github.com/Mokowz/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so"
+export LD_PRELOAD=/tmp/win.so
 
