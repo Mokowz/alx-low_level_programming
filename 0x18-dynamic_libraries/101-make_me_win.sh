c2b040ae79c6bec309d03821c8b1a9a0c0269b7f
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-15 05:00:57 -0800

    Add link

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P /tmp "https://github.com/Mokowz/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so"
+wget -q -O /tmp/win.so "https://github.com/Mokowz/alx-low_level_programming/blob/master/0x18-dynamic_libraries/win.so"
 export LD_PRELOAD=/tmp/win.so
 
